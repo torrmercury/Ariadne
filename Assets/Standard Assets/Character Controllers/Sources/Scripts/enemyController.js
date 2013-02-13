8189bf8a3124aea15354b7297abb35199b35946e
William Katz <wfk213@nyu.edu>
2013-02-13 11:52:07 -0500

    enemy behavior
    enemy now looks at and follows you

--- Assets/Standard Assets/Character Controllers/Sources/Scripts/enemyController.js
+++ Assets/Standard Assets/Character Controllers/Sources/Scripts/enemyController.js
@@ -1,31 +1,22 @@
     var distance;
     var target : Transform;
-    var lookAtDistance = 15.0;
     var attackRange = 10.0;
     var moveSpeed = 10.0;
     var damping = 6.0;
-    private var isItAttacking = false;
      
     function Update (){
    		distance = Vector3.Distance(target.position, transform.position);
-   		Debug.Log("Distance to player: "+distance);
-	    if(distance < 1000){
-	    	lookAt();
-	    	follow();
-	   }
+   		//Debug.Log("Distance to player: "+distance);
+	    follow();
+	   
     }
      
-    //orientates enemy toward player 
-    function lookAt (){
-    	var rotation = Quaternion.LookRotation(target.position - transform.position);
-   		transform.rotation = Quaternion.Slerp(transform.rotation, rotation, Time.deltaTime * damping);
-   		
-
-    }
+    
     //enemy moves toward player
     function follow(){
-    	distance = Vector3.Distance(target.position, transform.position);
-    	Debug.Log("Distance:"+distance);
-   		transform.Translate(moveSpeed * distance * Time.deltaTime); 
+    	//distance = Vector3.Distance(target.position, transform.position);
+    	distance = target.position - transform.position;
+    	//Debug.Log("Distance:"+distance);
+   		transform.Translate(moveSpeed *Vector3(0,0,1)* Time.deltaTime); 
     }
      
