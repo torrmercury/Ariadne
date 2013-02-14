d759d1252edf5a2a66e96515c738c134b77002e7
Zhan Huang <vh.noir@gmail.com>
2013-02-14 11:16:47 -0500

    fixed wall collision

--- Assets/PlayerBehaviourScript.js
+++ Assets/PlayerBehaviourScript.js
@@ -1,20 +1,21 @@
 var speed : int = 30;
 
 function Update () {
+	var controller : CharacterController = GetComponent(CharacterController);
 	if (Input.GetKey (KeyCode.UpArrow)){
 		transform.rotation = Quaternion.LookRotation(Vector3(0, 0, 1));
-		transform.Translate (Vector3(0,0,1) * Time.deltaTime*speed);
+		controller.Move(Vector3(0,0,1) * Time.deltaTime*speed);
 	}
 	if (Input.GetKey (KeyCode.DownArrow)){
 		transform.rotation = Quaternion.LookRotation(Vector3(0, 0, -1));
-		transform.Translate (Vector3(0,0,1) * Time.deltaTime*speed);
+		controller.Move(Vector3(0,0,-1) * Time.deltaTime*speed);
 	}
 	if (Input.GetKey (KeyCode.LeftArrow)){
 		transform.rotation = Quaternion.LookRotation(Vector3(-1, 0, 0));
-		transform.Translate (Vector3(0,0,1) * Time.deltaTime*speed);
+		controller.Move(Vector3(-1,0,0) * Time.deltaTime*speed);
 	}
 	if (Input.GetKey (KeyCode.RightArrow)){
 		transform.rotation = Quaternion.LookRotation(Vector3(1, 0, 0));
-		transform.Translate (Vector3(0,0,1) * Time.deltaTime*speed);
+		controller.Move(Vector3(1,0,0) * Time.deltaTime*speed);
 	}
 }
