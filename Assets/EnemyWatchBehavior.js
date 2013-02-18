72ee4f76e74a390d5cb013758fc383b341fa2e9c
Zhan Huang <vh.noir@gmail.com>
2013-02-18 13:43:26 -0500

    added enemy prefab that chases players in line of sight

--- Assets/EnemyWatchBehavior.js
+++ Assets/EnemyWatchBehavior.js
@@ -2,6 +2,7 @@
 
 var player1: Transform;
 var player2: Transform;
+var speed = 10;
 
 function Start () {
 
@@ -11,16 +12,39 @@ function Update () {
 	var hit : RaycastHit;
     var rayDirection1 = player1.position - transform.position;
     var rayDirection2 = player2.position - transform.position;
- 
-    if (Physics.Raycast (transform.position, rayDirection1, hit)) {
+ 	var hit1 = false;
+ 	var hit2 = false;
+ 	var hitDistance1 = Vector3.zero.magnitude;
+ 	var hitDistance2 = Vector3.zero.magnitude;
+ 	var controller : CharacterController = GetComponent(CharacterController);
+ 	
+ 	
+    if (Physics.Raycast(transform.position, rayDirection1, hit)) {
         if (hit.transform == player1) {
-            var hitDistance1 = (player1.position - transform.position);
+    		hit1 = true;
+    		Debug.Log("HIT 1");
+            hitDistance1 = Vector3.Distance(player1.position, transform.position);
         }
     }
-    if (Physics.Raycast (transform.position, rayDirection1, hit)) {
+    if (Physics.Raycast(transform.position, rayDirection2, hit)) {
         if (hit.transform == player2) {
-            
+	    	hit2 = true;
+	    	Debug.Log("HIT 2");
+            hitDistance2 = Vector3.Distance(player2.position, transform.position);
         }
     }
     
+    if(hit1){
+    	if(hit2){
+    		if(hitDistance1 < hitDistance2){
+    			controller.Move((player1.position - transform.position).normalized * Time.deltaTime * speed);
+    		} else{
+    			controller.Move((player2.position - transform.position).normalized * Time.deltaTime * speed);
+    		}
+    	} else{
+    		controller.Move((player1.position - transform.position).normalized * Time.deltaTime * speed);
+    	}
+    }else if(hit2){
+    	controller.Move((player2.position - transform.position).normalized * Time.deltaTime * speed);
+    } 
 }
