5568a12b6418d3fa3c358b36b3efe6d131fbc99f
William Katz <wfk213@nyu.edu>
2013-02-20 11:20:00 -0500

    gate prefab
    gate and gate switch prefab inside

---
+++ Assets/DoorScript1.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using System.Collections;
+
+public class DoorScript1 : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+		
+	
+	void openSesame(){
+		Debug.Log ("open sesame");
+		Destroy (gameObject);
+		
+	}
+}
---
+++ Assets/Standard Assets/Character Controllers/Sources/Scripts/DoorSwitch.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using System.Collections;
+
+public class DoorSwitch : MonoBehaviour {
+	
+	
+	public GameObject door;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	public void OnTriggerEnter(Collider other){
+		door.SendMessage("openSesame");
+		Destroy (this);
+	}
+}
