05b509d8d2761edfee6f2cd06a455ee8443a2b2a
Zhan Huang <vh.noir@gmail.com>
2013-02-20 13:28:40 -0500

    added joystick control

---
+++ Assets/JoystickLook.js
@@ -0,0 +1,27 @@
+var xSpeed = 250.0;
+var ySpeed = 120.0;
+ 
+private var x = 0.0;
+private var y = 0.0;
+ 
+@script AddComponentMenu("Camera-Control/Mouse Orbit")
+ 
+function Start () {
+    var angles = transform.eulerAngles;
+    x = angles.y;
+    y = angles.x;
+ 
+    // Make the rigid body not change rotation
+    if (rigidbody)
+        rigidbody.freezeRotation = true;
+}
+ 
+function LateUpdate () {
+ 
+        x += Input.GetAxis("Joy X") * xSpeed * 0.02;
+        y += Input.GetAxis("Joy Y") * ySpeed * 0.02;
+ 
+        var rotation = Quaternion.Euler(y, x, 0);
+ 
+        transform.rotation = rotation;
+}
