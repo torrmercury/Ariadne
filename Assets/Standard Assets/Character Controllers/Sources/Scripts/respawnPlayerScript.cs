03c9c45879eccaf8fb17f8857589532f2fd8c134
William Katz <wfk213@nyu.edu>
2013-04-25 12:37:32 -0400

    final level network

--- Assets/Standard Assets/Character Controllers/Sources/Scripts/respawnPlayerScript.cs
+++ Assets/Standard Assets/Character Controllers/Sources/Scripts/respawnPlayerScript.cs
@@ -5,7 +5,7 @@ public class respawnPlayerScript : MonoBehaviour {
 	
 	//variables set outside
 	public GameObject goal;
-	public GameObject player1;
+	public static GameObject player1;
 	public GameObject player2;
 	
 	//bite textures
---
+++ Assets/lobby.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+ 
+public class lobby : MonoBehaviour
+{
+	public bool first = false;
+    // Use this for initialization
+    void Start()
+    {
+        PhotonNetwork.ConnectUsingSettings("v1.0");
+    }
+ 
+    void OnGUI()
+    {
+        GUILayout.Label(PhotonNetwork.connectionStateDetailed.ToString());
+    }
+	void OnJoinedLobby()
+	{
+		PhotonNetwork.JoinRandomRoom();
+		
+	}
+	void OnPhotonRandomJoinFailed()
+	{
+		first = true;
+	    PhotonNetwork.CreateRoom(null);
+	}
+	void OnJoinedRoom()
+	{
+		if ( first){
+			
+			GameObject monster = PhotonNetwork.Instantiate("_Player1Controller", new Vector3(-32.0f,1.25f,-54.0f), Quaternion.identity, 0);
+			CharacterControl controller = monster.GetComponent<CharacterControl>();
+    controller.enabled = true;
+    	//	Camera camera = monster.GetComponent<Camera>();
+			
+    		//camera.enabled = true;
+			//monster.camera = enabled;
+		}
+		else { 
+			GameObject monster = PhotonNetwork.Instantiate("_Player2Controller", new Vector3(-34.0f,1.25f,-54.0f), Quaternion.identity, 0);
+			CharacterControl controller = monster.GetComponent<CharacterControl>();
+    controller.enabled = true;
+			//monster.camera = enabled;
+	
+		}
+	}
+}
