a65adba88df66bef883c2812f5f45dd61badf4eb
Zhan Huang <vh.noir@gmail.com>
2013-05-06 03:10:08 -0400

    added trap animation, replaced ghost sphere with fire skull

---
+++ Assets/Scripts Folder/TrapBehavior.js
@@ -0,0 +1,21 @@
+#pragma strict
+
+function Start () {
+	for (var state : AnimationState in animation) {
+    	state.speed = 4;
+	}
+}
+
+function Update () {
+
+}
+
+function OnTriggerEnter (other : Collider) {
+	if(other.tag == "Player1" || other.tag == "Player2")
+    	animation.Play("Up");
+}
+
+function OnTriggerExit (other : Collider) {
+	if(other.tag == "Player1" || other.tag == "Player2")
+    	animation.Play("Down");
+}
